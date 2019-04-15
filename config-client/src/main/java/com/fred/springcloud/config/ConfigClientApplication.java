19d59ad1d1af6bffb624eeb3fce6c50226227ca2
81673211 <81673211@qq.com>
2019-04-15 15:21:34 +0800

    init config

---
+++ config-client/src/main/java/com/fred/springcloud/config/ConfigClientApplication.java
@@ -0,0 +1,34 @@
+package com.fred.springcloud.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * <b>Description:.</b><br> 
+ * @author <b>sil.zhou</b>
+ * <br><b>ClassName:</b> 
+ * <br><b>Date:</b> 2019/4/14 16:23
+ */
+@RestController
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+    @Value("${name}")
+    private String name;
+
+    @RequestMapping("/hi")
+    public String hi() {
+        return name;
+
+    }
+}
